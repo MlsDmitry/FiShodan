2c26eebdb92d0aef12a992f8e62012d7aa8ed305
Rakabidaasta <kvist_tatyana@mail.ru>
2021-08-06 12:38:11 +0300

    Change design

--- fish_shodan/static/js/index.js
+++ fish_shodan/static/js/index.js
@@ -1,10 +1,10 @@
-const btn = document.getElementById('submit-btn')
+const btn = document.getElementById('btn__submit')
 const form = document.getElementById('main_form');
 
 async function sendForm() {
     const fd = new FormData(form)
     console.log(JSON.stringify(Object.fromEntries(fd)))
-    let r = await fetch('/tasks', {
+    let r = await fetch('/tasks/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -13,7 +13,7 @@ async function sendForm() {
         //     'single_domain': 'example.com'
         // }),
     })
-    .then(response => response.json())
+    // .then(response => response.json())
     .then(data => {
         console.log('Success:', data);
         getStatus(data.data.task_id)
@@ -21,11 +21,21 @@ async function sendForm() {
     .catch((error) => {
         console.log('Error: ', error)
     })
-
 }
-btn.addEventListener('click', function () {
-    sendForm()
-})
+
+function getData() {
+  var inpDomain = document.getElementById("input__domain");
+  var inpFile = document.getElementById("input__file");
+  if (inpDomain.value === "" && inpFile.value === "") {
+      console.log(inpDomain.value);
+      inpDomain.classList.add("warning"); 
+      console.log(inpFile.value);
+  } else {
+      inpDomain.classList.remove("warning");  
+      sendForm();
+      window.location.replace("./tasks/index.html");
+  }
+}
 
 function getStatus(taskID) {
     fetch(`/tasks/${taskID}`, {
@@ -34,7 +44,7 @@ function getStatus(taskID) {
         'Content-Type': 'application/json'
       },
     })
-    .then(response => response.json())
+    .then(response => okey())
     .then(res => {
         console.log(res.data)
       setTimeout(function() {
