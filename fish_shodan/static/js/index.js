0d85eafbbd45c43d5044b70018d91c6abd803916
Rakabidaasta <kvist_tatyana@mail.ru>
2021-08-06 14:20:38 +0300

    Solve merge conflicts again...

--- fish_shodan/static/js/index.js
+++ fish_shodan/static/js/index.js
@@ -1,4 +1,4 @@
-import axios from ''
+// import axios 
 
 // const axios = require('axios').default;
 
@@ -6,39 +6,55 @@ const btn = document.getElementById('submit-btn')
 const form = document.getElementById('main_form');
 
 async function sendForm() {
-    const fd = new FormData(form)
-    
-    axios.post('/tasks', {
-        method: 'POST',
-        data: {
-            'domain_single': 'example.com',
-        }
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-    
-    // let r = await fetch('/tasks', {
-    //     method: 'POST',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //     }
-    //     // body: JSON.stringify({
-    //     //     'single_domain': 'example.com'
-    //     // }),
-    // })
-    // .then(response => response.json())
-    // .then(data => {
-    //     console.log('Success:', data);
-    //     getStatus(data.data.task_id)
-    // })
-    // .catch((error) => {
-    //     console.log('Error: ', error)
-    // })
+  // const fd = new FormData(form)
+  
+  // axios.post('/tasks', {
+  //     method: 'POST',
+  //     data: {
+  //         'domain_single': 'example.com',
+  //     }
+  //   })
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
 
+  let r = await fetch('/tasks', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      }
+      // body: JSON.stringify({
+      //     'single_domain': 'example.com'
+      // }),
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Success:', data);
+      getStatus(data.data.task_id)
+  })
+  .catch((error) => {
+      console.log('Error: ', error)
+  })
+}
+
+function getData() {
+  var inpDomain = document.getElementById("input__domain");
+  var inpFile = document.getElementById("input__file");
+  if (inpDomain.value === "" && inpFile.value === "") {
+      console.log(inpDomain.value);
+      inpDomain.classList.add("warning"); 
+      console.log(inpFile.value);
+  } else {
+      inpDomain.classList.remove("warning");  
+      sendForm();
+  }
+}
+
+function okey() {
+  console.log("aaaaaaa")
 }
 
 
@@ -49,7 +65,7 @@ function getStatus(taskID) {
         'Content-Type': 'application/json'
       },
     })
-    .then(response => response.json())
+    .then(response => okey())
     .then(res => {
         console.log(res.data)
       setTimeout(function() {
