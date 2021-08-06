8a1a5f158878c4ef0bab38fb9870edb09c498c89
MlsDmitry <molokovich.dima@gmail.com>
2021-08-06 14:14:21 +0300

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit c0fde5c187617345ada871c27053310978b21985, reversing
    changes made to c5a88eef78f6f57e500bbdcf9a261458c1771e9c.

--- fish_shodan/static/js/index.js
+++ fish_shodan/static/js/index.js
@@ -6,38 +6,38 @@ const btn = document.getElementById('submit-btn')
 const form = document.getElementById('main_form');
 
 async function sendForm() {
-  const fd = new FormData(form)
-  
-  axios.post('/tasks', {
-      method: 'POST',
-      data: {
-          'domain_single': 'example.com',
-      }
-    })
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  
-  // let r = await fetch('/tasks', {
-  //     method: 'POST',
-  //     headers: {
-  //         'Content-Type': 'application/json'
-  //     }
-  //     // body: JSON.stringify({
-  //     //     'single_domain': 'example.com'
-  //     // }),
-  // })
-  // .then(response => response.json())
-  // .then(data => {
-  //     console.log('Success:', data);
-  //     getStatus(data.data.task_id)
-  // })
-  // .catch((error) => {
-  //     console.log('Error: ', error)
-  // })
+    const fd = new FormData(form)
+    
+    axios.post('/tasks', {
+        method: 'POST',
+        data: {
+            'domain_single': 'example.com',
+        }
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+    
+    // let r = await fetch('/tasks', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    //     // body: JSON.stringify({
+    //     //     'single_domain': 'example.com'
+    //     // }),
+    // })
+    // .then(response => response.json())
+    // .then(data => {
+    //     console.log('Success:', data);
+    //     getStatus(data.data.task_id)
+    // })
+    // .catch((error) => {
+    //     console.log('Error: ', error)
+    // })
 
 }
 
