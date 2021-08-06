7469d916523827f09ff2a0cefe80535a57ee67ac
MlsDmitry <molokovich.dima@gmail.com>
2021-08-06 18:59:45 +0300

    final js fix

--- fish_shodan/static/js/index.js
+++ fish_shodan/static/js/index.js
@@ -5,35 +5,21 @@
 const btn = document.getElementById('submit-btn')
 const form = document.getElementById('main_form');
 
-async function sendForm() {
-  // const fd = new FormData(form)
-  
-  // axios.post('/tasks', {
-  //     method: 'POST',
-  //     data: {
-  //         'domain_single': 'example.com',
-  //     }
-  //   })
-  //   .then(function (response) {
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
+async function sendForm(ips) {
 
   let r = await fetch('/tasks', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
-      }
-      // body: JSON.stringify({
-      //     'single_domain': 'example.com'
-      // }),
+      },
+      body: JSON.stringify({
+          'domains': JSON.stringify(ips)
+      }),
   })
   .then(response => response.json())
   .then(data => {
       console.log('Success:', data);
-      getStatus(data.data.task_id)
+      getStatus(data.task_ids)
   })
   .catch((error) => {
       console.log('Error: ', error)
@@ -52,28 +38,69 @@ function getData() {
       const fileList = inpFile.files[0];
       console.log(fileList);  
       
-      sendForm();
+      var ips = []
+      if (inpDomain != "") {
+        ips.push(inpDomain)
+      }
+      if (inpFile.files.length) {
+        var reader = new FileReader()
+        var data = reader.readAsText(inpFile.files[0])
+        console.log(data)
+        data.split("\n").forEach((el) => {
+          ips.push(el)
+        })
+      }
+      sendForm(ips)
   }
 }
 
-function okey() {
-  console.log("aaaaaaa")
+async function okey(resp) {
+  var data = await resp
+  var task_ids = []
+  for (var key in data) {
+    insertData(data[key])
+    if (data[key]["task_status"] != "SUCCESS") {
+      task_ids.push(key)
+    }
+  }
+
+  setTimeout(function() {
+
+    getStatus(task_ids);
+  }, 5000);
+}
+
+function insertData(results) {
+  var list = document.getElementsByClassName("list__tasks")[0];
+  // var res = JSON.parse(results);
+  var htmlCode = "";
+
+  
+  htmlCode += `<div class="task">
+      <div class="task__header">
+          Id таска: ${ results["task_id"] }
+      </div>
+      <div class="task__status">
+          Статус: ${ results["task_status"] }
+      </div>
+      <div class="task__result">
+          Результат: ${ results["task_result"] }
+      </div>
+  </div>`
+
+  list.innerHTML += htmlCode
+  
 }
 
 
-function getStatus(taskID) {
-    fetch(`/tasks/${taskID}`, {
+function getStatus(taskIDs) {
+  console.log("Task ids are: " + taskIDs.join(';'))
+    fetch(`/tasks/${taskIDs.join(';')}`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json'
       },
     })
-    .then(response => okey())
-    .then(res => {
-        console.log(res.data)
-      setTimeout(function() {
-        getStatus(res.data.task_id);
-      }, 1000);
-    })
+    .then(response => okey(response.json()))
     .catch(err => console.log(err));
-  }
+}
